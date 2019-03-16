c1dff498bfda19a8fa72763c1bc8bd00862acb36
lmarson <lmarson@kth.se>
2019-03-16 13:25:38 +0100

    changes

--- cacca.py
+++
@@ -1,6 +0,0 @@
-import tensorflow as tf
-
-for example in tf.python_io.tf_record_iterator("/media/luca/backup/out250/train-00000-of-00016"):
-    result = tf.train.Example.FromString(example)
-
-    print(result)
